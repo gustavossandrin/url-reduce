8a96ff925738ee6fa1d970832c9bd868b565c9f4
Pichau <gustavosandrindattein123@gmail.com>
2022-07-14 20:59:51 -0300

    implementado a contagem de cliques por pagina

--- devpro/encurtador/models.py
+++ devpro/encurtador/models.py
@@ -11,3 +11,16 @@ class UrlRedirect(models.Model):
 
     def __str__(self):
         return f'UrlRedirect para {self.destino}'
+
+
+class UrlLog(models.Model):
+    criado_em = models.DateTimeField(auto_now_add=True)
+    origem = models.URLField(max_length=512, null=True, blank=True)
+    user_agent = models.CharField(max_length=512, null=True, blank=True)
+    host = models.CharField(max_length=512, null=True, blank=True)
+    ip = models.GenericIPAddressField(null=True, blank=True)
+    url_redirect = models.ForeignKey(UrlRedirect, models.DO_NOTHING, related_name='logs')
+
+    def __str__(self):
+        iso = self.criado_em.isoformat()
+        return f'{iso}: {self.origem}'
